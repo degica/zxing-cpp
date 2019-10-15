ed384e50d88c83b033d47d59e39c206d5b626faa
Nigel Baillie <nbaillie@degica.com>
2019-10-15 10:53:28 +0900

    remove git module and cmake

--- ext/zxing/extconf.rb
+++ ext/zxing/extconf.rb
@@ -3,13 +3,6 @@ require 'mkmf'
 ZXING_CPP = "#{File.dirname(__FILE__)}/zxing-cpp"
 ZXING_CPP_BUILD = "#{ZXING_CPP}/build"
 
-`cmake --version` rescue raise "zxing_cpp.rb installation requires cmake"
-
-Dir.mkdir ZXING_CPP_BUILD unless File.exist? ZXING_CPP_BUILD
-Dir.chdir ZXING_CPP_BUILD do
-  `cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_CXX_FLAGS="-fPIC" ..`
-end
-
 Dir.chdir ZXING_CPP_BUILD do
   `make`
 end
