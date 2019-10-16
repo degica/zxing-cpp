8d7c1d13b170509b89cbc4966a0f68ab943c8cb6
Nigel Baillie <nbaillie@degica.com>
2019-10-16 14:08:38 +0900

    Bundle cmake

--- ext/zxing/extconf.rb
+++ ext/zxing/extconf.rb
@@ -1,7 +1,13 @@
 require 'mkmf'
 
-ZXING_CPP = "#{File.dirname(__FILE__)}/zxing-cpp"
+ZXING_CPP = "#{__dir__}/zxing-cpp"
 ZXING_CPP_BUILD = "#{ZXING_CPP}/build"
+ZXING_CMAKE = "#{__dir__}/cmake/bin/cmake"
+
+Dir.mkdir ZXING_CPP_BUILD unless File.exist? ZXING_CPP_BUILD
+Dir.chdir ZXING_CPP_BUILD do
+  `#{ZXING_CMAKE} -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_CXX_FLAGS="-fPIC" ..`
+end
 
 Dir.chdir ZXING_CPP_BUILD do
   `make`
